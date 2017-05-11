0000418a23345da6f5ecc718094dc6ae13b70f5e
Joshua Camacho <joshuajcamacho@gmail.com>
2017-05-11 13:51:06 -0700

    working

--- src/gift/grid/problem/GiftGridProblem.java
+++ src/gift/grid/problem/GiftGridProblem.java
@@ -5,90 +5,95 @@
  */
 package gift.grid.problem;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.Map;
+import java.util.Set;
 
 /**
  *
  * @author Josh
  */
 public class GiftGridProblem {
- static int R = 5, C = 5;
-static int memo[][] = new int[R][C];
-static int curMax=0;
-static LinkedList<String> dir;
-static String[][] directions = new String[R][C];
+
+static Map dirMap; 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        dir=new LinkedList<String>();
-        for (int r=0 ; r != R ; r++){
-            for (int c = 0 ; c != C ; c++){
-                memo[r][c] = -1;
-                directions[r][c]="";
-            }
-                
-        }   
+        dirMap = new HashMap<String, String>();
+       
+         
         int field[][] = {
-            {1,    50,100,3,4},
-            {10000, 90, 20,3,10},
-            {1,2,9002,1,2},
-            {1,2,3,4,5},
-            {1,2,3,4,5}
+            {1,     50, 30},
+            {10000, 90, 32},
+            {100,   299,91},
+          
         };
         
-         int res = max(0, 0, field);
-         System.out.println("\n"+res);
-         
-         for(int i=0; i<dir.size(); i++){
-             System.out.println(dir.get(i));
-         }
-         System.out.println(directions[0][0]);
-         for (int r=0 ; r != R ; r++){
-            for (int c = 0 ; c != C ; c++){
-                System.out.print(memo[r][c]+" ");
-                System.out.print(directions[r][c]+" ");
-            }
-           System.out.println("\n");
-        } 
-    }
-   
-
-
-    public static int max(int r, int c, int[][] field) {
-        if(r==R || c==C ) return 0; 
-        if (memo[r][c] != -1) {
-            System.out.println("\nBASECASE r was "+r+" c was "+c+"memo was "+memo[r][c]);
-            return memo[r][c];
-        }
-        int down = 0, right = 0;
-        if (r != R) down = max(r+1, c, field);
-        if (c != C) right = max(r, c+1, field);
-        int m = Math.max(down, right);
+         int max = getMaxValue(field);
+         System.out.println("\n"+max);
+         System.out.println("---Path----");
+         printRoute(dirMap);
         
-//        System.out.print("\nr is "+r +" c is "+c);
-//        
-//            System.out.print(" Down was " +down);
-//        
-//            System.out.print("\n Right was " +right);
-//            System.out.print("\nm wwas "+m);
-        memo[r][c] = (field[r][c] + m);
-        String dire="";
-        if(m==down) dire="Down";
-        else if(m==right) dire="Right";
-        directions[r][c]=directions[r][c].concat(dire+ " ");
-//        System.out.println("\nmemo[r][c] was+" +memo[r][c]);
-//        if(true){
-//            if(curMax<memo[r][c]){
-//                curMax=memo[r][c];
-//                if(down>right) dir.push("Down");
-//                if(right>down) dir.push("Right");
-//            }
-//        }
-        return memo[r][c];
+//// 
     }
-    
+  
     
 
+     public static int getMaxValue(int[][] values) {
+        int rows = values.length;
+        int cols = values[0].length;
+ 
+        int[] maxValues = new int[cols];
+        for(int i = 0; i < rows; ++i) {
+            for(int j = 0; j < cols; ++j) {
+                int left = 0;
+                int up = 0;
+ 
+                if(i > 0) {
+                    up = maxValues[j];
+                }
+ 
+                if(j > 0) {
+                    left = maxValues[j - 1];
+                }
+                String di="";
+                int m = Math.max(left, up);
+                int o=i;
+                int p=j;
+                if(left != 0 && left==m){
+                    di="right";
+                    p--;
+                }else if(up!=0){
+                    di="down";
+                    o--;
+                }
+                dirMap.put("("+o+","+p+")", di+ " to ("+i+","+j+")");
+               
+                maxValues[j] = Math.max(left, up) + values[i][j];
+            }
+        }
+        
+        return maxValues[cols - 1];
+    }
+
+    private static void printRoute(Map dirMap) {
+        
+        System.out.println("(0,0) "+ dirMap.get("(0,0)"));
+        String full = (String) dirMap.get("(0,0)");
+        String coord = full.substring(full.length() - 5);
+        while(dirMap.containsKey(coord)){
+            
+            full = (String) dirMap.get(coord);
+            System.out.println(coord +" "+ full);
+            coord = full.substring(full.length() - 5);
+            
+        }
+    }
+    
 }
